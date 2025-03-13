4c60bc61e12262531d645570048818461b372c48
adithyasn11 <adithyasn2487@gmail.com>
2025-03-13 19:08:45 +0530

    fix: replace Card component with TCard in Team Page

--- app/Team-Page/page.jsx
+++ app/Team-Page/page.jsx
@@ -110,7 +110,7 @@ const Page = () => {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
             {filteredMembers.map((member) => (
-              <Card
+              <TCard
               key={member.id}
               name={member.name}
               imageUrl={member.photoUrl} 
